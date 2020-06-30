c197fe9195fac6229da304634675920e3354ad81
farookjintha <farookjintha@gmail.com>
2020-06-30 19:00:38 +0530

    product update added

--- controller/product.js
+++ controller/product.js
@@ -94,3 +94,57 @@ exports.remove = (req, res) =>{
         });
     });
 }
+
+//To update a product
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err){
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            res.status(400).json({
+                error: "All fields are required."
+            });
+        }
+
+
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        //File Size
+        // 1 kb = 1000
+        // 1 mb = 1000000
+
+
+        if(files.photo){
+
+            // If the photo size is more that 1 mb
+            if(files.photo.size > 1000000){
+                res.status(400).json({
+                    error : "File size exceeded. Image should be less than 1 MB size."
+                });
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+
+            res.json(result);
+        });
+    });
+}
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, productById, read, remove } = require('../controller/product');
+const { create, productById, read, remove, update } = require('../controller/product');
 const { requireSignIn, isAuth, isAdmin } = require('../controller/auth');
 const { userById } = require('../controller/user');
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update);
 
 
 router.param('userId', userById);
