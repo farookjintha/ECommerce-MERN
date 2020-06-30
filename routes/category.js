0fd90809573bcc1a9f2c24cb70173c5e8de28b84
farookjintha <farookjintha@gmail.com>
2020-06-30 19:59:27 +0530

    reading category added

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const { create } = require('../controller/category');
+const { create, categoryById, read } = require('../controller/category');
 const { requireSignIn, isAuth, isAdmin } = require('../controller/auth');
 const { userById } = require('../controller/user');
 
 
 const router = express.Router();
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignIn, isAuth, isAdmin, create);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
